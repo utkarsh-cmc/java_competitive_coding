6049d0f185703516fd43a07778700ddaea671b73
Utkarsh Gumphekar <54374420+utkarsh-cmc@users.noreply.github.com>
2021-07-03 22:37:17 +0530

    star problems in java

---
+++ Star/src/mainClass.java
@@ -0,0 +1,23 @@
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		
+for(int i=0;i<=5;i++)
+{
+	for(int j=0;j<=i;j++)
+	{
+		if(i==j||i==5)
+		{
+			System.out.print("*");
+		}
+		else
+		{
+			System.out.print(" ");
+		}
+	}
+		System.out.println();
+}
+	
+	}}
+	
